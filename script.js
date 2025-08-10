91e7eb3c8fa2ddcf40f28bc793232658d950e2d7
profrose26 <145849997+profrose26@users.noreply.github.com>
2025-08-10 09:48:45 -0300

    proximo passo

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ cconst avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
+         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
      })
 })

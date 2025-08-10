0f29537f3654cfbbe8ffd39786313f55be2723b1
profrose26 <145849997+profrose26@users.noreply.github.com>
2025-08-10 09:45:44 -0300

    atual

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const avanca = document.querySelectorAll('.btn-proximo');
+cconst avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+     })
+})
